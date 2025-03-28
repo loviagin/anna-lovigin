8099bde7e60c0c1f4208b609e819c1fad92773ed
Ilia <ilia.loviagin@gmail.com>
2025-03-28 16:11:32 +0300

    New Home

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Отключает проверку ESLint при сборке
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
